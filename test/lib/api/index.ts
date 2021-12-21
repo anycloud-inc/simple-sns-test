3c518264d9906fa0e58d578634f154225e6fa3af
KazutakaShimizu <s.kazutaka55555@gmail.com>
2021-12-21 19:11:23 +0900

    paramSerializerをaxiosの初期化時に設定するように

--- test/lib/api/index.ts
+++ test/lib/api/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import * as qs from 'qs'
 import { addAuthorizationHeader } from './request'
 
 const baseURL = process.env.API_URL
@@ -6,5 +7,6 @@ const validateStatus = (status: number) => status >= 200 && status < 500
 const api = axios.create({ baseURL, validateStatus })
 api.defaults.headers.common['Content-Type'] = 'application/json'
 api.interceptors.request.use(addAuthorizationHeader)
+api.defaults.paramsSerializer = (params: any) => qs.stringify(params)
 
 export default api
