3b2627e31df0f4f71ac3016f93e6cb7dccf43df3
KazutakaShimizu <s.kazutaka55555@gmail.com>
2021-12-22 20:17:46 +0900

    markAsReadを修正

--- test/features/mark-as-read.test.ts
+++ test/features/mark-as-read.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'power-assert'
-import { createRoom, signup } from '../lib/test-helper'
+import { createMessage, createRoom, signup } from '../lib/test-helper'
 import { roomRepository } from '../repositories/room.repository'
 
 export async function markAsRead() {
@@ -8,7 +8,8 @@ export async function markAsRead() {
   await signup()
   const user1 = await signup()
   const user2 = await signup()
-  let room = await createRoom([user1.id, user2.id])
+  let room = await createRoom([user1.id!])
+  await createMessage(room.id)
 
   let response = await roomRepository.markAsRead(room.id)
   assert.equal(response.status, 200, '正しい値なので、200が返ってくるべき')
@@ -16,9 +17,18 @@ export async function markAsRead() {
   response = await roomRepository.findOne(room.id)
   room = response.data.room
   const roomUser = room.roomUsers.find(roomUser => roomUser.userId === user2.id)
+
   assert.notEqual(
-    roomUser.readAt,
+    roomUser?.readAt,
     null,
     '既読がついていれば、roomUser.readAtに値が入ってるべき'
+  )
+
+  await signup()
+  response = await roomRepository.findOne(room.id)
+  assert.equal(
+    response.status,
+    404,
+    '自分が所属しないRoomをとってこようとしてるので、404が返ってくるべき'
   )
 }
