5bc737064d7bc3a240e7264d00622b95c137cdf2
KazutakaShimizu <s.kazutaka55555@gmail.com>
2021-12-22 10:40:27 +0900

    メッセージの取得テスト

---
+++ test/features/find-messages.test.ts
@@ -0,0 +1,38 @@
+import * as assert from 'power-assert'
+import { Message } from '../entities/message.entity'
+import { PaginationParams } from '../lib/pagination'
+import { createMessage, createRoom, signup } from '../lib/test-helper'
+import { messageRepository } from '../repositories/message.repository'
+
+export async function findMessages() {
+  console.log('find messages')
+
+  await signup()
+  const user1 = await signup()
+  const user2 = await signup()
+  const room = await createRoom([user1.id, user2.id])
+  const message1 = await createMessage(room.id)
+  const message2 = await createMessage(room.id)
+
+  let messages = await _findMessages(room.id)
+  assert.equal(
+    messages[0].id,
+    message2.id,
+    '直前に作ったMessageと最新のMessageのidが一致するべき'
+  )
+
+  messages = await _findMessages(room.id, { cursor: message2.id })
+  assert.equal(
+    messages[0].id,
+    message1.id,
+    'cursorにidを指定したMessageのひとつ前のMessageが、最新のMessageになるべき'
+  )
+}
+
+async function _findMessages(
+  roomId: string,
+  pagination?: PaginationParams
+): Promise<Message[]> {
+  const response = await messageRepository.find(roomId, pagination)
+  return response.data.messages
+}
--- test/lib/test-helper.ts
+++ test/lib/test-helper.ts
@@ -1,11 +1,13 @@
 import { Post } from '../entities/post.entity'
 import { User } from '../entities/user.entity'
 import { Room } from '../entities/room.entity'
+import { Message } from '../entities/message.entity'
 import { userFactory } from '../factories/user.factory'
 import { accountRepository } from '../repositories/account.repository'
 import { postRepository } from '../repositories/post.repository'
 import { roomRepository } from '../repositories/room.repository'
 import { AccessToken } from './access-token'
+import { messageRepository } from '../repositories/message.repository'
 
 export async function signup(): Promise<User> {
   const response = await accountRepository.create(userFactory.create())
@@ -21,4 +23,9 @@ export async function createPost(): Promise<Post> {
 export async function createRoom(userIds: number[]): Promise<Room> {
   const response = await roomRepository.create(userIds)
   return response.data.room
+}
+
+export async function createMessage(roomId: string): Promise<Message> {
+  const response = await messageRepository.create(roomId, 'test message')
+  return response.data.message
 }
